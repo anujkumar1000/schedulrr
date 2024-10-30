05b509d223e8d1e7985649631e2a17ba13e0903c
anuj <anujku10mar@gmail.com>
2024-10-30 15:08:40 +0530

    Deployment

---
+++ app/(main)/availability/layout.jsx
@@ -0,0 +1,12 @@
+import {Suspense} from "react";
+
+
+export default function AvailabilityLayout({children}) {
+    return (
+        <div className="mx-auto">
+      <Suspense fallback={<div>Loading availability...</div>}>
+        {children}
+      </Suspense>
+      </div>
+    );
+  }
---
+++ app/not-found.jsx
@@ -0,0 +1,15 @@
+// export default function NotFound() {
+//     return (
+//       <div className="text-4xl font-extrabold w-screen pt-96 grid place-items-center">
+//         <h1>404 - Page Not Found</h1>
+//       </div>
+//     );
+//   }
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-extrabold">404 - Page Not Found</h1>
+    </div>
+  );
+}
